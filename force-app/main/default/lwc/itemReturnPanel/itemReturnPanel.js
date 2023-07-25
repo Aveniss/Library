493679165e630cec12ed766390abed25e2fda1dd
Aveniss <kamilst934@gmail.com>
2023-07-25 15:43:14 +0200

    PS-014 fix

--- force-app/main/default/lwc/itemReturnPanel/itemReturnPanel.js
+++ force-app/main/default/lwc/itemReturnPanel/itemReturnPanel.js
@@ -5,8 +5,8 @@
 import { LightningElement } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-import getLoans from "@salesforce/apex/ShowTheLoanController.getLoans";
-import updateLoans from "@salesforce/apex/ShowTheLoanController.updateLoans";
+import getLoans from "@salesforce/apex/ItemReturnPanelController.getLoans";
+import updateLoans from "@salesforce/apex/ItemReturnPanelController.updateLoans";
 import EmailLabel from "@salesforce/label/c.EmailLabel";
 import EmailPlaceholder from "@salesforce/label/c.EmailPlaceholder";
 import SelectStatusLabel from "@salesforce/label/c.SelectStatusLabel";
@@ -27,9 +27,7 @@ const RESERVATION = "Reservation";
 const RETURNED = "Returned";
 const ALL = "All";
 
-export default class ShowTheLoan extends LightningElement {
-  // eslint-disable-next-line @lwc/lwc/no-document-query
-  _filteredLoans = [];
+export default class ItemReturnPanel extends LightningElement {
   allLoans = [];
 
   loanStatus = ALL;
@@ -49,18 +47,29 @@ export default class ShowTheLoan extends LightningElement {
   };
 
   get filteredLoans() {
-    return this._filteredLoans;
+    return this.allLoans
+      .map((element) => ({
+        ...element,
+        Style: element.Selected ? SELECTED_DIV_STYLE : UNSELECTED_DIV_STYLE
+      }))
+      .filter(
+        (element) =>
+          element.Status__c === this.loanStatus || this.loanStatus === ALL
+      );
   }
 
   get getNumberOfSelectedLoans() {
-    return this.numberOfSelectedLoans;
+    return this.allLoans.filter((element) => element.Selected).length;
   }
 
   get options() {
     return [
       { label: ALL, value: ALL },
       { label: RESERVATION, value: RESERVATION },
-      { label: BORROWED, value: BORROWED },
+      {
+        label: BORROWED,
+        value: BORROWED
+      },
       { label: RETURNED, value: RETURNED },
       { label: DELAY, value: DELAY }
     ];
@@ -71,40 +80,26 @@ export default class ShowTheLoan extends LightningElement {
     if (this.allLoans.length === 0) {
       this.loadData();
     }
-    this.filterData();
   }
 
   handleChangeEmail(event) {
     this.email = event.detail.value;
   }
 
-  filterData() {
-    this._filteredLoans.length = 0;
-    this._filteredLoans = this.allLoans.filter((element) => {
-      return element.Status__c === this.loanStatus || this.loanStatus === ALL;
-    });
-  }
-
   loadData() {
     this.allLoans.length = 0;
     getLoans({ email: this.email, status: ALL })
       .then((result) => {
-        let key = 0;
-        result.forEach((element) => {
-          let newElement = JSON.parse(JSON.stringify(element));
-          newElement.Selected = false;
-          newElement.Key = key;
-          newElement.Style = UNSELECTED_DIV_STYLE;
-          this.allLoans.push(newElement);
-          key += 1;
-        });
-        this.filterData();
+        this.allLoans = result.map((element) => ({
+          ...element,
+          Selected: false
+        }));
       })
-      .catch((error) => {
+      .catch((downloadError) => {
         this.dispatchEvent(
           new ShowToastEvent({
             title: "Data Download Failed",
-            message: error.message,
+            message: downloadError.message,
             variant: "error"
           })
         );
@@ -112,36 +107,31 @@ export default class ShowTheLoan extends LightningElement {
   }
 
   updateItemsStatus() {
-    let selectedLoans = this.allLoans.filter((element) => {
-      return element.Selected;
-    });
+    let selectedLoans = this.allLoans.filter((element) => element.Selected);
 
-    updateLoans({ loans: selectedLoans });
-
-    this._filteredLoans.length = 0;
-    this.allLoans.length = 0;
-
-    // eslint-disable-next-line no-restricted-globals
-    location.reload();
+    updateLoans({ loans: selectedLoans })
+      .then(() => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+      })
+      .catch((udateError) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Data Update Failed",
+            message: udateError.body.message,
+            variant: "error"
+          })
+        );
+      });
   }
 
   selectItem(event) {
     if (this.loanStatus === DELAY || this.loanStatus === BORROWED) {
       const passedData = event.target.dataset.id;
-      const div = this.template.querySelector(`div[data-id="${passedData}"]`);
-      const divID = Number(div.id.split("-")[0]);
-      let loan = this.allLoans.filter((element) => {
-        return element.Key === divID;
-      })[0];
+      let loan = this.allLoans.find((element) => element.Id === passedData);
 
-      if (loan.Selected) {
-        loan.Style = UNSELECTED_DIV_STYLE;
-        this.numberOfSelectedLoans -= 1;
-      } else {
-        loan.Style = SELECTED_DIV_STYLE;
-        this.numberOfSelectedLoans += 1;
-      }
       loan.Selected = !loan.Selected;
+      this.allLoans = this.allLoans.map((element) => ({ ...element }));
     }
   }
 }
