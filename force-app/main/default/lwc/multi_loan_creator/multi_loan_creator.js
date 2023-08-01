7da9be8d90993db3cc2d00cc0ec45b11950cf629
Aveniss <kamilst934@gmail.com>
2023-08-01 12:01:09 +0200

    PS-015 final commit

--- force-app/main/default/lwc/multi_loan_creator/multi_loan_creator.js
+++ force-app/main/default/lwc/multi_loan_creator/multi_loan_creator.js
@@ -2,19 +2,47 @@
  * Created by kamil on 07.07.2023.
  */
 
-import { LightningElement } from "lwc";
+import { api, LightningElement } from "lwc";
 import getItems from "@salesforce/apex/MultiLoanCreatorController.getItems";
+import createLoans from "@salesforce/apex/MultiLoanCreatorController.createLoans";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
+import SearchedDatatableLabel from "@salesforce/label/c.SearchedDatatableLabel";
+import SelectedDatatable from "@salesforce/label/c.SelectedDatatable";
+import SelectStatusLabel from "@salesforce/label/c.SelectStatusLabel";
+import DateFieldLabel from "@salesforce/label/c.DateFieldLabel";
+import ItemNameLabel from "@salesforce/label/c.ItemNameLabel";
+import SelectTypeLabel from "@salesforce/label/c.SelectTypeLabel";
+
 export default class ShowTheLoan extends LightningElement {
+  label = {
+    SelectTypeLabel,
+    SearchedDatatableLabel,
+    SelectedDatatable,
+    SelectStatusLabel,
+    DateFieldLabel,
+    ItemNameLabel
+  };
+
+  @api recordId;
+  itemName = "";
+  itemType = "";
+  loanStatus = "";
+  endOfLoan = "";
+
   searchedItems = [];
   selectedItems = [];
   columns = [
     { label: "Name", fieldName: "Name", type: "text" },
-    { label: "Type", fieldName: "Type__c", type: "text" },
+    {
+      label: "Type",
+      fieldName: "Type__c",
+      type: "text"
+    },
     { label: "Genre", fieldName: "Genre__c", type: "text" }
   ];
-  get options() {
+
+  get itemTypes() {
     return [
       { label: "Paper Book", value: "Paper Book" },
       { label: "Magazine", value: "Magazine" },
@@ -25,12 +53,47 @@ export default class ShowTheLoan extends LightningElement {
     ];
   }
 
-  itemName = "";
-  itemType = "";
+  handleDateChange(event) {
+    const selectedDate = new Date(event.target.value);
+    const today = new Date();
+
+    if (selectedDate <= today) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: "Invalid Date",
+          message: "The date cannot be earlier than tomorrow",
+          variant: "error"
+        })
+      );
+      event.target.value = "";
+    } else {
+      this.endOfLoan = selectedDate;
+    }
+  }
+
+  get checkButtonStatus() {
+    return !(
+      this.selectedItems.length !== 0 &&
+      this.endOfLoan !== "" &&
+      this.loanStatus !== ""
+    );
+  }
+
+  handleLoanStatusChange(event) {
+    this.loanStatus = event.target.value;
+  }
+
+  get newStatusOptions() {
+    return [
+      { label: "Reservation", value: "Reservation" },
+      { label: "Borrowed", value: "Borrowed" }
+    ];
+  }
 
   handleChangeType(event) {
     this.itemType = event.detail.value;
   }
+
   handleChangeName(event) {
     this.itemName = event.detail.value;
   }
@@ -38,21 +101,30 @@ export default class ShowTheLoan extends LightningElement {
   get getSearchedItems() {
     return this.searchedItems;
   }
+
   get getSelectedItems() {
     return this.selectedItems;
   }
+
   changeOnSelect(event) {
     const selectedRows = event.detail.selectedRows;
     if (selectedRows.length) {
       this.searchedItems = this.searchedItems.map((element) => {
-        console.log(element.Id + " " + selectedRows[0].Id);
-
-        let found = selectedRows.some((item) => item.Id === element.Id);
-        console.log("wynik metody: " + found);
-        return found ? { ...element, selected: true } : element;
+        return selectedRows.some((item) => item.Id === element.Id)
+          ? { ...element, selected: true }
+          : element;
       });
+    }
+  }
 
-      console.log("cos poszlo " + selectedRows[0].selected);
+  changeOnUnselect(event) {
+    const selectedRows = event.detail.selectedRows;
+    if (selectedRows.length) {
+      this.selectedItems = this.selectedItems.map((element) => {
+        return selectedRows.some((item) => item.Id === element.Id)
+          ? { ...element, selected: false }
+          : element;
+      });
     }
   }
 
@@ -66,19 +138,28 @@ export default class ShowTheLoan extends LightningElement {
     );
   }
 
+  moveUnselectedItems() {
+    this.searchedItems = [
+      ...this.searchedItems,
+      ...this.selectedItems.filter((element) => !element.selected)
+    ];
+    this.selectedItems = this.selectedItems.filter(
+      (element) => element.selected
+    );
+  }
+
   searchNewItems() {
+    let allIDs = this.selectedItems.map((item) => item.Id);
     getItems({
       name: this.itemName,
       type: this.itemType,
-      genre: "",
-      additionalFiler: ""
+      existingIDs: allIDs
     })
       .then((result) => {
         this.searchedItems = result.map((element) => ({
           ...element,
           selected: false
         }));
-        console.log("result: " + this.searchedItems);
       })
       .catch((downloadError) => {
         this.dispatchEvent(
@@ -92,16 +173,24 @@ export default class ShowTheLoan extends LightningElement {
   }
 
   createNewLoans() {
-    console.log(
-      "Itemy: " +
-        this.searchedItems[0].selected +
-        " " +
-        this.searchedItems[0].Name +
-        " " +
-        this.searchedItems[0].Type__c +
-        " " +
-        this.searchedItems[0].Genre__c +
-        " "
-    );
+    createLoans({
+      borrowerId: this.recordId,
+      items: this.selectedItems,
+      endOfRental: this.endOfLoan,
+      status: this.loanStatus
+    })
+      .catch((insertError) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Data Insert Failed",
+            message: insertError.message,
+            variant: "error"
+          })
+        );
+      })
+      .then(() => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+      });
   }
 }
