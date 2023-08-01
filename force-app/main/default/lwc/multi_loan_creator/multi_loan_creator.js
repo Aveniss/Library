a6267b688ce7b594b81cd74ce83a3abd03df7078
Aveniss <kamilst934@gmail.com>
2023-08-01 18:11:28 +0200

    PS-015 adding labels

--- force-app/main/default/lwc/multi_loan_creator/multi_loan_creator.js
+++ force-app/main/default/lwc/multi_loan_creator/multi_loan_creator.js
@@ -2,13 +2,12 @@
  * Created by kamil on 07.07.2023.
  */
 
-import {api, LightningElement, track, wire} from "lwc";
+import { api, LightningElement, track, wire } from "lwc";
 import getItems from "@salesforce/apex/MultiLoanCreatorController.getItems";
 import createLoans from "@salesforce/apex/MultiLoanCreatorController.createLoans";
-import {ShowToastEvent} from "lightning/platformShowToastEvent";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import { RefreshEvent } from "lightning/refresh";
-import {getPicklistValues} from 'lightning/uiObjectInfoApi';
-
+import { getPicklistValues } from "lightning/uiObjectInfoApi";
 
 import SearchedDatatableLabel from "@salesforce/label/c.SearchedDatatableLabel";
 import SelectedDatatable from "@salesforce/label/c.SelectedDatatable";
@@ -22,8 +21,8 @@ import ITEM_FIELD_NAME from "@salesforce/schema/Item__c.Name";
 import ITEM_FIELD_GENRE from "@salesforce/schema/Item__c.Genre__c";
 import ITEM_FIELD_TYPE from "@salesforce/schema/Item__c.Type__c";
 
-import TYPE_FIELD from '@salesforce/schema/Item__c.Type__c';
-import STATUS_FIELD from '@salesforce/schema/Loan__c.Status__c';
+import TYPE_FIELD from "@salesforce/schema/Item__c.Type__c";
+import STATUS_FIELD from "@salesforce/schema/Loan__c.Status__c";
 
 import ItemLabelName from "@salesforce/label/c.ItemLabelName";
 import ItemLabelType from "@salesforce/label/c.ItemLabelType";
@@ -37,197 +36,189 @@ import ItemTypeErrorTitle from "@salesforce/label/c.ItemTypeErrorTitle";
 import LoanStatusErrorTitle from "@salesforce/label/c.LoanStatusErrorTitle";
 
 export default class ShowTheLoan extends LightningElement {
-    label = {
-        SelectTypeLabel,
-        SearchedDatatableLabel,
-        SelectedDatatable,
-        SelectStatusLabel,
-        DateFieldLabel,
-        ItemNameLabel
-    };
+  label = {
+    SelectTypeLabel,
+    SearchedDatatableLabel,
+    SelectedDatatable,
+    SelectStatusLabel,
+    DateFieldLabel,
+    ItemNameLabel
+  };
 
-    @api recordId;
-    itemName = '';
-    itemType = '';
-    loanStatus = '';
-    endOfLoan = '';
+  @api recordId;
+  itemName = "";
+  itemType = "";
+  loanStatus = "";
+  endOfLoan = "";
 
-    @track searchedItems = [];
-    @track selectedItems = [];
-    @track itemTypes = [];
-    @track newStatusOptions = [];
+  @track searchedItems = [];
+  @track selectedItems = [];
+  @track itemTypes = [];
+  @track newStatusOptions = [];
 
-    columns = [
-        {label: ItemLabelName, fieldName: ITEM_FIELD_NAME.fieldApiName},
-        {
-            label: ItemLabelType, fieldName: ITEM_FIELD_TYPE.fieldApiName
-        },
-        {label: ItemLabelGenre, fieldName: ITEM_FIELD_GENRE.fieldApiName}
-    ];
+  columns = [
+    { label: ItemLabelName, fieldName: ITEM_FIELD_NAME.fieldApiName },
+    {
+      label: ItemLabelType,
+      fieldName: ITEM_FIELD_TYPE.fieldApiName
+    },
+    { label: ItemLabelGenre, fieldName: ITEM_FIELD_GENRE.fieldApiName }
+  ];
 
-    @wire(getPicklistValues, {
-        recordTypeId: DefaultRecordTypeId,
-        fieldApiName: TYPE_FIELD
-    })
-    getTypePicklistValues({error, data}) {
-        if (data) {
-            this.itemTypes = data.values;
-        } else if (error) {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: ItemTypeErrorTitle,
-                    message: error.message,
-                    variant: "error"
-                })
-            );
-        }
-    }
-
-    @wire(getPicklistValues, {
-        recordTypeId: DefaultRecordTypeId,
-        fieldApiName: STATUS_FIELD
-    })
-    getStatusPicklistValues({error, data}) {
-        if (data) {
-            this.newStatusOptions = data.values;
-        } else if (error) {
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: LoanStatusErrorTitle,
-                    message: error.message,
-                    variant: "error"
-                })
-            );
-        }
-    }
-
-    handleDateChange(event) {
-        this.endOfLoan = event.target.value;
-    }
-
-    get getMinimumDate() {
-        let currentDate = new Date();
-        currentDate.setDate(currentDate.getDate() + 1);
-        return currentDate.toISOString().slice(0, 10);
-    }
-
-    get checkButtonStatus() {
-        return !(
-            this.selectedItems.length && this.endOfLoan && this.loanStatus
-        );
-    }
-
-    handleLoanStatusChange(event) {
-        this.loanStatus = event.target.value;
-    }
-
-
-    handleChangeType(event) {
-        this.itemType = event.detail.value;
-    }
-
-    handleChangeName(event) {
-        this.itemName = event.detail.value;
-    }
-
-    get getSearchedItems() {
-        return this.searchedItems;
-    }
-
-    get getSelectedItems() {
-        return this.selectedItems;
-    }
-
-    changeOnSelect(event) {
-        const selectedRows = event.detail.selectedRows;
-        if (selectedRows.length) {
-            this.searchedItems = this.searchedItems.map((element) => {
-                return selectedRows.some((item) => item.Id === element.Id)
-                    ? {...element, selected: true}
-                    : element;
-            });
-        }
-    }
-
-    changeOnUnselect(event) {
-        const selectedRows = event.detail.selectedRows;
-        if (selectedRows.length) {
-            this.selectedItems = this.selectedItems.map((element) => {
-                return selectedRows.some((item) => item.Id === element.Id)
-                    ? {...element, selected: false}
-                    : element;
-            });
-        }
-    }
-
-    moveSelectedItems() {
-        this.selectedItems = [
-            ...this.selectedItems,
-            ...this.searchedItems.filter((element) => element.selected)
-        ];
-        this.searchedItems = this.searchedItems.filter(
-            (element) => !element.selected
-        );
-    }
-
-    moveUnselectedItems() {
-        this.searchedItems = [
-            ...this.searchedItems,
-            ...this.selectedItems.filter((element) => !element.selected)
-        ];
-        this.selectedItems = this.selectedItems.filter(
-            (element) => element.selected
-        );
-    }
-
-    searchNewItems() {
-        let allIDs = this.selectedItems.map((item) => item.Id);
-        getItems({
-            name: this.itemName,
-            type: this.itemType,
-            existingIDs: allIDs
+  @wire(getPicklistValues, {
+    recordTypeId: DefaultRecordTypeId,
+    fieldApiName: TYPE_FIELD
+  })
+  getTypePicklistValues({ error, data }) {
+    if (data) {
+      this.itemTypes = data.values;
+    } else if (error) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: ItemTypeErrorTitle,
+          message: error.message,
+          variant: "error"
         })
-            .then((result) => {
-                this.searchedItems = result.map((element) => ({
-                    ...element,
-                    selected: false
-                }));
-            })
-            .catch((downloadError) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: DataDownloadFailedTitle,
-                        message: downloadError.message,
-                        variant: "error"
-                    })
-                );
-            });
+      );
     }
+  }
 
-    createNewLoans() {
-        createLoans({
-            borrowerId: this.recordId,
-            items: this.selectedItems,
-            endOfRental: this.endOfLoan,
-            status: this.loanStatus
-        }).then(() => {
-            this.selectedItems.length = 0;
-            this.dispatchEvent(new RefreshEvent());
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: DataProcessingSucceeded,
-                    message: DataProcessingSucceededMessage,
-                    variant: "success"
-                })
-            );
-        }).catch((insertError) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: DataInsertFailedTitle,
-                        message: insertError.message,
-                        variant: "error"
-                    })
-                );
-            })
+  @wire(getPicklistValues, {
+    recordTypeId: DefaultRecordTypeId,
+    fieldApiName: STATUS_FIELD
+  })
+  getStatusPicklistValues({ error, data }) {
+    if (data) {
+      this.newStatusOptions = data.values;
+    } else if (error) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: LoanStatusErrorTitle,
+          message: error.message,
+          variant: "error"
+        })
+      );
     }
+  }
+
+  handleDateChange(event) {
+    this.endOfLoan = event.target.value;
+  }
+
+  get getMinimumDate() {
+    let currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1);
+    return currentDate.toISOString().slice(0, 10);
+  }
+
+  get checkButtonStatus() {
+    return !(this.selectedItems.length && this.endOfLoan && this.loanStatus);
+  }
+
+  handleLoanStatusChange(event) {
+    this.loanStatus = event.target.value;
+  }
+
+  handleChangeType(event) {
+    this.itemType = event.detail.value;
+  }
+
+  handleChangeName(event) {
+    this.itemName = event.detail.value;
+  }
+
+  changeOnSelect(event) {
+    const selectedRows = event.detail.selectedRows;
+    if (selectedRows.length) {
+      this.searchedItems = this.searchedItems.map((element) => {
+        return selectedRows.some((item) => item.Id === element.Id)
+          ? { ...element, selected: true }
+          : element;
+      });
+    }
+  }
+
+  changeOnUnselect(event) {
+    const selectedRows = event.detail.selectedRows;
+    if (selectedRows.length) {
+      this.selectedItems = this.selectedItems.map((element) => {
+        return selectedRows.some((item) => item.Id === element.Id)
+          ? { ...element, selected: false }
+          : element;
+      });
+    }
+  }
+
+  moveSelectedItems() {
+    this.selectedItems = [
+      ...this.selectedItems,
+      ...this.searchedItems.filter((element) => element.selected)
+    ];
+    this.searchedItems = this.searchedItems.filter(
+      (element) => !element.selected
+    );
+  }
+
+  moveUnselectedItems() {
+    this.searchedItems = [
+      ...this.searchedItems,
+      ...this.selectedItems.filter((element) => !element.selected)
+    ];
+    this.selectedItems = this.selectedItems.filter(
+      (element) => element.selected
+    );
+  }
+
+  searchNewItems() {
+    let allIDs = this.selectedItems.map((item) => item.Id);
+    getItems({
+      name: this.itemName,
+      type: this.itemType,
+      existingIDs: allIDs
+    })
+      .then((result) => {
+        this.searchedItems = result.map((element) => ({
+          ...element,
+          selected: false
+        }));
+      })
+      .catch((downloadError) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: DataDownloadFailedTitle,
+            message: downloadError.message,
+            variant: "error"
+          })
+        );
+      });
+  }
+
+  createNewLoans() {
+    createLoans({
+      borrowerId: this.recordId,
+      items: this.selectedItems,
+      endOfRental: this.endOfLoan,
+      status: this.loanStatus
+    })
+      .then(() => {
+        this.selectedItems.length = 0;
+        this.dispatchEvent(new RefreshEvent());
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: DataProcessingSucceeded,
+            message: DataProcessingSucceededMessage,
+            variant: "success"
+          })
+        );
+      })
+      .catch((insertError) => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: DataInsertFailedTitle,
+            message: insertError.message,
+            variant: "error"
+          })
+        );
+      });
+  }
 }
